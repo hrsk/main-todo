ef60202af7ec15541fa59273305f15e835450f34
hrsk <hrsk.yegor@gmail.com>
2025-09-11 19:06:19 +0300

    feat(ui): snadcn, layout todo card

--- src/pages/to-do-list/List.tsx
+++ src/pages/to-do-list/List.tsx
@@ -1,3 +1,34 @@
+import { Button } from "@/components/ui/button.tsx";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card.tsx";
+
 export const List = () => {
-    return <div>TO-DO LIST</div>;
+    return (
+        <Card className={"flex min-w-2.5 max-w-md"}>
+            <CardHeader>
+                <CardTitle>Todolist title</CardTitle>
+                <CardAction>
+                    <Button variant={"secondary"}>x</Button>
+                </CardAction>
+                <CardDescription>
+                    Date created: {new Date().getFullYear()}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <span>Tasks</span>
+            </CardContent>
+            <CardFooter>
+                <Button variant={"default"}>ALL</Button>
+                <Button variant={"outline"}>ACTIVE</Button>
+                <Button variant={"outline"}>COMPLETED</Button>
+            </CardFooter>
+        </Card>
+    );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+import path from "path";
+
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), tailwindcss()],
     server: {
         port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
 });
